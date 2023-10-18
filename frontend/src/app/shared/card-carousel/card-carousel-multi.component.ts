5ce67bed08c4e130971ff72dd7f9caf3a8baffeb
Johan <johanvillabarbera@gmail.com>
2023-10-18 20:07:30 +0200

    Estilos carousel

--- frontend/src/app/shared/card-carousel/card-carousel-multi.component.ts
+++ frontend/src/app/shared/card-carousel/card-carousel-multi.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-carousel-multi',
@@ -7,7 +9,20 @@ import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 })
 export class CardCarouselMultiComponent implements OnInit{
 
-constructor(){ }
+constructor(private router: Router,
+  private formBuilder: FormBuilder){ 
+    this.form = this.formBuilder.group({
+      limit: '',
+      offset: '',
+      title: '',
+      categories: [],
+      price_min: '',
+      price_max: '',
+      order: ''
+    });
+  }
+
+  form!: FormGroup
 
   dataTest?: any[]
 
@@ -18,5 +33,12 @@ constructor(){ }
 
   ngOnInit(): void { }
 
-
+  redirectShop(category: string){
+    this.form.get('categories')?.setValue([category])
+    const DATA_FORM = this.form.value;
+    console.log(DATA_FORM);
+    
+    const encodeFormGroupFilter = btoa(JSON.stringify(DATA_FORM)); // Se codifican los filtros
+    this.router.navigateByUrl('/shop/' + `?filters=${encodeFormGroupFilter}`); // Se almacenan en la url
+  }
 }
--- frontend/src/app/shared/cmp-products/list-products-infinite/list-products-infinite.component.ts
+++ frontend/src/app/shared/cmp-products/list-products-infinite/list-products-infinite.component.ts
@@ -1,3 +1,4 @@
+
 import { Component, OnInit } from '@angular/core';
 import { Producto, ProductosService } from 'src/app/core';
 
@@ -10,6 +11,7 @@ export class ListProductsInfiniteComponent implements OnInit{
 
   products: Producto[] = [];
   offset: number = 0;
+  count: number = 0;
 
   constructor( private productsService: ProductosService ) {
 
@@ -31,16 +33,20 @@ export class ListProductsInfiniteComponent implements OnInit{
   getProducts() {
     const QUANTITY_PRODUCTS = 3;
     const params = this.getRequestParams(this.offset, QUANTITY_PRODUCTS);
+    if(this.count < 2) {
+      this.productsService.getInfinite(params).subscribe(
+        (data) => {
+          this.products = this.products.concat(data.products);
+          this.offset = this.offset + 3;
+        },
+        (error) => {
+          // Mostrar error
+        }
+      )
+    }
+    
 
-    this.productsService.getInfinite(params).subscribe(
-      (data) => {
-        this.products = this.products.concat(data.products);
-        this.offset = this.offset + 3;
-      },
-      (error) => {
-        // Mostrar error
-      }
-    )
+    this.count++;
 
   }
 

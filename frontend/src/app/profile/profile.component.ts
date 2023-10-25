cea1ebfba824d0e7baffaa90c0c31cc24e9eff19
miguelvidalbellido <mbellido13@gmail.com>
2023-10-25 21:04:16 +0200

    Profile

--- frontend/src/app/profile/profile.component.ts
+++ frontend/src/app/profile/profile.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User, UserService } from '../core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit{
+  
+  userLogged?: User;
+  user?: String;
+  isYourProfile?: boolean;
+  constructor(
+    private _userService: UserService,
+    private _route: ActivatedRoute
+  ) {
+
+  }
+
+  ngOnInit(): void {
+ 
+    this._route.url.subscribe(data => {
+      this.user = data[0].path
+    })
+
+    this._userService.currentUser.subscribe(data => {
+      this.userLogged = data;
+      
+      this.isYourProfile = (this.userLogged.username === this.user)
+      ? true
+      : false
+      
+    });
+
+  }
 
 }
